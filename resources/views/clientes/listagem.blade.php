18219bbd7dd821047e6ec6bc9722e6528cfa7cba
Vinicius Oliver <viniciusoliver.dev@gmail.com>
2025-11-01 12:41:22 -0300

    Atualizacao

---
+++ resources/views/clientes/listagem.blade.php
@@ -0,0 +1,5 @@
+@extends("layouts.admin-app")
+
+@section("content")
+    <h1>Listagem de cliented</h1>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,6 +68,7 @@
         </main>
     </div>
     <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/popper.min.js') }}"></script>
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
     <script src="{{ asset('js/adminlte.min.js') }}"></script>
 </body>
